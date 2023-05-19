89fb20acc6d4886dedca0b044c08597066314e96
Диаконашвили Зураб Нодарович <zdiakonashvili@sportmaster.ru>
2023-05-19 23:30:53 +0300

    01-basics\01-CounterButtonApp

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,10 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      counter: 0
+    };
+  }
+}).mount('#app');
