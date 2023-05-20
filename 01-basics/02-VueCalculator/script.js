4669e48b5a4e5d5ee624703f31e1530fa1eecc42
Диаконашвили Зураб Нодарович <zdiakonashvili@sportmaster.ru>
2023-05-20 19:09:09 +0300

    01-basics\02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const vm = createApp({
+  data() {
+    return {
+      arg1: null,
+      arg2: null,
+      operation: null,
+    }
+  },
+  computed: {
+    result() {
+      switch (this.operation) {
+        case 'sum':
+          return this.arg1 + this.arg2;
+        case 'subtract':
+          return this.arg1 - this.arg2;
+        case 'multiply':
+          return this.arg1 * this.arg2;
+        case 'divide':
+          return this.arg1 / this.arg2;
+      };
+    },
+  },
+}).mount('#app');
+
+window.vm = vm;
